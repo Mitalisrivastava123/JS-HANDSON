69b947d8d733b6c4d274c152cb14622cdf613c16
Mitalisri <mitalisrivastava@cedcoss.com>
2022-09-26 11:37:40 +0530

    JS VARIABLES EXERICES 1-5

--- src/script.js
+++ src/script.js
@@ -1,2 +1,20 @@
 // assigning value to a variable
 var carName="Volvo";
+
+// defining variable x with value 50
+var x = 50;
+
+//  Adding variable x and y using concatenation operator
+var x=5;
+var y=10;
+document.getElementById("demo").innerHTML=x+y;
+
+// adding variables x and y and giving alert
+
+var x=5;
+var y=10;
+var z=x+y;
+alert(z);
+
+// declaring three variables on one line
+var firstName ="john",lastName="Doe",age=35;
