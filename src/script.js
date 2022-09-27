24b346bbad29255ca5046c4c924859bda2bc02a4
Mitalisri <mitalisrivastava@cedcoss.com>
2022-09-27 13:05:28 +0530

    JS BREAK LOOP 1-2

--- src/script.js
+++ src/script.js
@@ -291,3 +291,18 @@ while(i<10)
     i=i+2;
 }
 }
+// used the break loop stop when i is 5.
+for (i = 0; i < 10; i++) {
+    console.log(i);
+    if (i == 5) {
+     break; 
+    }
+  }
+
+// used the continue loop when i is 5
+for (i = 0; i < 10; i++) {
+    if (i == 5) {
+      continue;
+    }
+    console.log(i);
+  }
